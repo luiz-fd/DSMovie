e23727e0e6c81f28184c46be77ea450a31fd9018
Luiz F Dias <luizfd@gmail.com>
2023-06-20 14:08:38 -0300

    All UserServiceTests passed

--- src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
+++ src/test/java/com/devsuperior/dsmovie/services/ScoreServiceTests.java
@@ -1,9 +1,22 @@
 package com.devsuperior.dsmovie.services;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.ScoreDTO;
+import com.devsuperior.dsmovie.entities.ScoreEntity;
+import com.devsuperior.dsmovie.repositories.MovieRepository;
+import com.devsuperior.dsmovie.repositories.ScoreRepository;
+import com.devsuperior.dsmovie.tests.MovieFactory;
+import com.devsuperior.dsmovie.tests.ScoreFactory;
 
 @ExtendWith(SpringExtension.class)
 public class ScoreServiceTests {
@@ -11,8 +24,35 @@ public class ScoreServiceTests {
 	@InjectMocks
 	private ScoreService service;
 	
+	@Mock
+	private ScoreRepository scoreRepository;
+	
+	@Mock
+	private MovieRepository movieRepository;	
+
+	private Long existingMovieId;
+	private Long nonExistingMovieId;
+	private Long dependendMovieId;
+	private ScoreEntity scoreEntity;
+	private MovieDTO movieDTO;
+	private ScoreDTO scoreDTO;
+	
+	@BeforeEach
+	void setUp() {
+		existingMovieId = 1L;
+		nonExistingMovieId = 2L;
+		dependendMovieId = 3L;
+		scoreEntity = ScoreFactory.createScoreEntity();
+		movieDTO = MovieFactory.createMovieDTO();
+		scoreDTO = ScoreFactory.createScoreDTO();
+		
+		Mockito.when(scoreRepository.save(ArgumentMatchers.any())).thenReturn(scoreEntity);	
+	}
+	
 	@Test
 	public void saveScoreShouldReturnMovieDTO() {
+		MovieDTO result = service.saveScore(scoreDTO);
+		Assertions.assertNotNull(result);
 	}
 	
 	@Test
